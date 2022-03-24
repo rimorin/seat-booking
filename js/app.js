0c50adcf11faf05fbc470c5947d7d487335aeb46
Kwan Rimorin John Eric <je_macbook@JE-Macbook.local>
2022-03-24 14:52:06 +0800

    Add stats and better logic

--- js/app.js
+++ js/app.js
@@ -1,34 +1,29 @@
 $(document).ready(function () {
 
     const firebaseURL = "https://seat-booking-5f1b9-default-rtdb.firebaseio.com/";
-
     const firebase = new Firebase(firebaseURL);
-
     const adminUsers = ["admin"];
 
     Login = function () {
-
         var username = $("#emailId").val();
-
         if(!username){
             alert("Please enter name!!");
             return;
         }
-
         $("#resetSeat").css('visibility', 'hidden');
         if(adminUsers.indexOf(username) > -1) {
             $("#resetSeat").css('visibility', 'visible');
         }
         $("#seatStructure").css('pointer-events', 'auto');
-        $("h3").text(`Welcome ${username}`);
+        $("h5").html(`Welcome ${username} <br/>You can start booking your seats.<br/>Click on your existing bookings to cancel them.`);
         showSeats(username);
+        $("#emailId, #loginBtn").prop("disabled", true);
     };
 
     ResetBooking = function() {
         firebase.remove();
         window.location.reload();
     }
-
 
     setupSeats();
     showSeats("");
@@ -73,19 +68,20 @@ $(document).ready(function () {
                         });
                     }
                 });
-
             }
         }
-
     }
+
     function showSeats(username) {
         firebase.on("value", function (snapshot) {
             const allSeats = (snapshot.val());
+            let userCounter = {};
             for (var key in allSeats) {
-
                 let id = "#seat" + allSeats[key].locX + allSeats[key].locY;
                 const seatName = allSeats[key].name;
-
+                if(seatName) {
+                    userCounter[seatName] = userCounter[seatName] ? userCounter[seatName] + 1 : 1;
+                }
                 if(username) {
                     $(id).attr('src', 'images/none.png');
                     $(id).attr('style', "cursor: pointer");
@@ -105,6 +101,16 @@ $(document).ready(function () {
                     }
                 }
                 
+            }
+            let statsText = "";
+            $.each(userCounter, function(index,value){
+                statsText += `${index} booked ${value} seats <br/>`;
+            })
+            const statsElement = $("#seatStats");
+            if(statsText) {
+                statsElement.html(statsText);
+            } else {
+                statsElement.text("No bookings");
             }
         });
     }
