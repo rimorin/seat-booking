19f6f0e75ed85102135372373ae4fd03f333ca45
Kwan Rimorin John Eric <je_macbook@JE-Macbook.local>
2022-03-24 11:35:05 +0800

    First commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Realtime Seat Booking
+
+## Setup Instructions:
+
+   - Create firebase project
+   - Setup Realtime database
+   - Set public read and write access
+   - Copy generated firebase realtime db url and paste in `firebaseURL` constant variable.
+
+## How to use:
+
+   - Login with any username
+   - Click on the seats
+   - Seats will turn green to indicate reservation
+   - Click on the green seats again to remove reservation
+   - Login as "admin" to enable "Reset Seats" button for next meeting
+
+## Technologies Used
+
+   - Angular Js - Is a dependency for Firebase
+   - Firebase - Offers realtime database
+   - Sematic UI - To give a nice look to the UI
+   - jQuery - Is a dependency for Semantic UI
---
+++ js/app.js
@@ -0,0 +1,114 @@
+$(document).ready(function () {
+
+    const firebaseURL = "https://seat-booking-5f1b9-default-rtdb.firebaseio.com/";
+
+    const firebase = new Firebase(firebaseURL);
+
+    const adminUsers = ["admin"];
+
+
+    Login = function () {
+
+        var username = $("#emailId").val();
+
+        if(!username){
+            alert("Please enter name!!");
+            return;
+        }
+
+        $("#resetSeat").css('visibility', 'hidden');
+        if(adminUsers.indexOf(username) > -1) {
+            $("#resetSeat").css('visibility', 'visible');
+        }
+        $("#seatStructure").css('pointer-events', 'auto');
+        $("h3").text(`Welcome ${username}`);
+        showSeats(username);
+    };
+
+    ResetBooking = function() {
+        firebase.remove();
+        window.location.reload();
+    }
+
+
+    setupSeats();
+    showSeats("");
+
+
+    function setupSeats() {
+        // 0 - unbooked seat
+        // 1 - booked seat
+        let contentString = "";
+        // To load all the seats
+        for (let i = 0; i < 5; i++) {
+            contentString += "<div>"
+            for (let j = 0; j < 10; j++) {
+                contentString += "<img id='seat" + i + j + "' src='images/none.png' />";
+            }
+            contentString += "</div>";
+        }
+        $("#seats").html(contentString);
+
+        for (let i = 0; i < 10; i++) {
+            for (let j = 0; j < 10; j++) {
+                $('#seats').on('click', '#seat' + i + j, function () {
+
+                    let string = 'seat' + i + j;
+
+                    if ($('#seat' + i + j).attr('src') == 'images/booked.png') {
+                        // Do nothing
+                    }
+                    else if ($('#seat' + i + j).attr('src') == 'images/none.png') {
+                        firebase.child(string).set({
+                            vacancy: 1,
+                            name: $("#emailId").val(),
+                            locX: i,
+                            locY: j
+                        });
+                    } else {
+                        firebase.child(string).set({
+                            vacancy: 0,
+                            name: "",
+                            locX: i,
+                            locY: j
+                        });
+                    }
+                });
+
+            }
+        }
+
+    }
+    function showSeats(username) {
+        firebase.on("value", function (snapshot) {
+            const allSeats = (snapshot.val());
+            for (var key in allSeats) {
+
+                let id = "#seat" + allSeats[key].locX + allSeats[key].locY;
+                const seatName = allSeats[key].name;
+
+                if(username) {
+                    $(id).attr('src', 'images/none.png');
+                    $(id).attr('style', "cursor: pointer");
+                    if (seatName == username && allSeats[key].vacancy == 1) {
+                        $(id).attr('src', 'images/selected.png');
+                        $(id).attr('style', "cursor: pointer");
+                    }
+                    else if (seatName != username && allSeats[key].vacancy == 1) {
+                        $(id).attr('src', 'images/booked.png');
+                        $(id).attr('style', "cursor: default");
+                        
+                    }
+                } else {
+                    if (allSeats[key].vacancy == 1) {
+                        $(id).attr('src', 'images/booked.png');
+                        $(id).attr('title', seatName);
+                    }
+                }
+                
+            }
+        });
+    }
+});
+
+
