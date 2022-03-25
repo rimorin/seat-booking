fc7107c3cba2bbce96651f93f9b626f596f325f6
Kwan Rimorin John Eric <je_macbook@JE-Macbook.local>
2022-03-25 15:33:20 +0800

    WN as default

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 const firebaseURL = "https://seat-booking-5f1b9-default-rtdb.firebaseio.com/";
-const defaultGroupMaster = "ws";
+const defaultGroupMaster = "wn";
 const groupMapping = new Map([
     ["group-1", "Group 1"],
     ["group-2", "Group 2"],
